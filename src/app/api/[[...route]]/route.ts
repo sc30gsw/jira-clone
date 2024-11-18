b7f2342ad89df0b9086252bce5b8d3b67aebe086
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-18 21:23:33 +0900

    🚑 hono導入

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,23 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello Next.js!',
+  })
+})
+
+app.get('/project/:projectId', (c) => {
+  const { projectId } = c.req.param()
+
+  return c.json({
+    message: `Hello Next.js! ${projectId}`,
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)

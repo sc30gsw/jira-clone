a0c0dc0400686ac956ec0f8c80cb0e3cc3aee4bd
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-24 19:43:00 +0900

    ✨ apiエンドポイントに/workspacesを追加

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,4 +1,6 @@
 import auth from '@/features/auth/server/route'
+import workspaces from '@/features/workspaces/server/route'
+
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
@@ -6,7 +8,7 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-const routes = app.route('/auth', auth)
+const routes = app.route('/auth', auth).route('/workspaces', workspaces)
 
 export const GET = handle(app)
 export const POST = handle(app)

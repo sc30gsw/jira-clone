07ec86431a7f0717d52a4f6a07884c0436f160ae
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-25 17:45:53 +0900

    🚸 新規fetch時にローディング状態とわかるようにする対応

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,11 +1,22 @@
 'use client'
-
+import { Skeleton } from '@/components/ui/skeleton'
 import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces'
 import { CreateWorkspaceButton } from '@/features/workspaces/components/create-workspace-button'
 import { WorkspaceSelect } from '@/features/workspaces/components/workspace-select'
 
 export const WorkspaceSwitcher = () => {
-  const { data: workspaces } = useGetWorkspaces()
+  const { data: workspaces, isLoading } = useGetWorkspaces()
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-y-2">
+        <div className="flex items-center justify-between">
+          <Skeleton className="size-10 rounded-md" />
+          <Skeleton className="w-3/4 h-10" />
+        </div>
+      </div>
+    )
+  }
 
   if (!workspaces) {
     return null

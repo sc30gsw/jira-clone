d0a26720ad89b2034fbba370bc7608f234436124
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-11-25 17:28:55 +0900

    🚸 作成時に作成したワークスペースが選択されるようCSRに変更

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,17 +1,23 @@
+'use client'
+
+import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces'
+import { CreateWorkspaceButton } from '@/features/workspaces/components/create-workspace-button'
 import { WorkspaceSelect } from '@/features/workspaces/components/workspace-select'
-import { WorkspaceSelectContentItem } from '@/features/workspaces/components/workspace-select-content-item'
-import { RiAddCircleFill } from 'react-icons/ri'
 
 export const WorkspaceSwitcher = () => {
+  const { data: workspaces } = useGetWorkspaces()
+
+  if (!workspaces) {
+    return null
+  }
+
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
         <p className="text-xs uppercase text-neutral-500">Workspaces</p>
-        <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
+        <CreateWorkspaceButton />
       </div>
-      <WorkspaceSelect>
-        <WorkspaceSelectContentItem />
-      </WorkspaceSelect>
+      <WorkspaceSelect workspaces={workspaces.documents} />
     </div>
   )
 }
